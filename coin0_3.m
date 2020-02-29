15d62870790f0f8ab684dbf2798e55e712f81349
Yurihou <46369018+Yurihou@users.noreply.github.com>
2020-02-29 02:02:22 -0600

    Upgrade hw2 code.

--- coin0_3.m
+++ coin0_3.m
@@ -1,11 +1,17 @@
-function[rt]=coin0_3()
+function[rt]=coin0_3(pro)
     r=rand(1,1000);
     z=zeros(1,1000);
     n=zeros(1,1000);
     f=zeros(1,1000);
     t=1:1000;
+    if pro>1
+        pro=1;
+    end
+    if pro<0
+        pro=0;
+    end
     for i=1:1000
-        if r(i)>0.3
+        if r(i)>(1-pro)
             z(i)=1;
         else
             z(i)=0;
@@ -17,7 +23,7 @@ function[rt]=coin0_3()
       n(i)=a;
       f(i)=a/i;
     end
-    scatter(t,f,2)    %if use coin0_3_100, please remove this line.
-    axis([0,1000,0,1])
+    scatter(t,f,2)
+    axis([0,1000,0,1]);
     rt=f(1000);    
 end
--- coin0_3_100.m
+++ coin0_3_100.m
@@ -1,7 +1,8 @@
-k=zeros(1,1000);
-for i=1:1000
-    k(i)=coin0_3;
+pro=0.5;
+k=zeros(1,100);
+for i=1:100
+    k(i)=coin0_3(pro);
 end
-t=1:1000;
+t=1:100;
 scatter(t,k,2)
-axis([0,1000,0,1])
+axis([0,100,0,1])
